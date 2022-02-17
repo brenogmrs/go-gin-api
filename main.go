1dfeb7c379e25617c8fbfa73f4320d122957bd66
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-02-17 16:12:51 -0300

    feat: implement StartDB method

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/brenogmrs/go-gin-api/server"
+import (
+	"github.com/brenogmrs/go-gin-api/database"
+	"github.com/brenogmrs/go-gin-api/server"
+)
 
 func main() {
+
+	database.StartDB()
+
 	server := server.NewServer()
 
 	server.Run()
