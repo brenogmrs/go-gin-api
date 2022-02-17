3be27a0e198c0b2a81e1e8c5e7ac3d6d3c8621bb
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-02-17 16:48:45 -0300

    feat: add get book by id method

--- controllers/bookController.go
+++ controllers/bookController.go
@@ -1,9 +1,40 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/brenogmrs/go-gin-api/database"
+	"github.com/brenogmrs/go-gin-api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func ShowBook(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+
+	id := c.Param("id")
+
+	newId, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be an integer",
+		})
+		return
+	}
+
+	db := database.GetDatabase()
+
+	var foundBook models.Book
+
+	err = db.First(&foundBook, newId).Error
+
+	if err != nil {
+		c.JSON(404, gin.H{
+			"error": "Book not found" + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, foundBook)
+
 }
